295b1ecce6b61cb5e3245f0a5857d34bc3e85b5f
Reuben Bond <rebond@microsoft.com>
2023-02-01 15:21:56 -0800

    Initial commit with support for Map<K,V> from Language-Ext

---
+++ Orleans.Serialization.Support.LanguageExt/Map.cs
@@ -0,0 +1,23 @@
+﻿using LanguageExt;
+
+namespace Orleans.Serialization.Support.LanguageExt;
+
+[RegisterConverter]
+public class MapConverter<K, V> : IConverter<Map<K, V>, MapSurrogate<K, V>>
+{
+    public Map<K, V> ConvertFromSurrogate(in MapSurrogate<K, V> surrogate) => new Map<K, V>(surrogate.Items);
+
+    public MapSurrogate<K, V> ConvertToSurrogate(in Map<K, V> value)
+    {
+        var items = new List<(K Key, V Value)>(value.Count);
+        items.AddRange(value.Pairs);
+        return new MapSurrogate<K, V> { Items = items };
+    }
+}
+
+[GenerateSerializer]
+public struct MapSurrogate<K, V>
+{
+    [Id(0)]
+    public List<(K Key, V Value)> Items { get; set; }
+}
